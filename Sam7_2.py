42ad952697d7498034bbb23ae306b80353950737
Kostya Bushuev <ksbushuev@gmail.com>
2023-10-02 11:14:27 +0500

    Подготовлен файл для Самостоятельной работе 8

--- Sam7_2.py
+++
@@ -1,9 +0,0 @@
-with open('./static/Sam7_2.txt', 'a') as file:
-    date = input('Введите дату: ')
-    category = input('Введите категорию: ')
-    sum = int(input('Введите сумму: '))
-
-    file.write(f'{date}\t\t\t{category}\t\t\t{sum}\n')
-
-with open('./static/Sam7_2.txt', 'r') as file:
-    print('Учёт расходов:\n', file.read())
