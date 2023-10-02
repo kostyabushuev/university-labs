9e5de3dd8201fabfc85ddd11aec61b38740a6bc9
Kostya Bushuev <ksbushuev@gmail.com>
2023-10-02 11:27:52 +0500

    Сделано задание 8 в Самостоятельной работе 8

---
+++ Sam8_1.py
@@ -0,0 +1,9 @@
+class Animal:
+    def __init__(self, name, weight):
+        self.name = name
+        self.weight = weight
+
+my_animal = Animal('Koshka', 10)
+
+print(my_animal.name)
+print(my_animal.weight)
